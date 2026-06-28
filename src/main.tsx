6c25555b349cf121e738f9e889c70dfe8205e3d8
trevorthanhtung <trevorthanhtung@github.com>
2026-06-28 23:17:58 +0700

    Revert "chore: host fonts locally to prevent FOUT"
    This reverts commit b5242c0b15f5a5f5b7982c907636c5ff19456bfb.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "@fontsource-variable/plus-jakarta-sans";
-import "@fontsource-variable/bricolage-grotesque";
 import "./styles.css";
 import "./i18n";
 import { startNotificationService } from "./utils/notifications";
